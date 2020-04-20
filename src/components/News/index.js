be0e5178b99f4123b6f1337bcf3ad204dbe1117f
baptiste <baptiste.bordet@gmail.com>
2020-04-20 22:46:06 +0200

    add aws s3 provider

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -13,7 +13,7 @@ const News = () => {
                     return newsItems.map(({ titre, description, lien_youtube, image }, index) => {
                         return (
                             <div key={`news-${index}`} className="news-item">
-                                {image && <img src={`${process.env.REACT_APP_BACKEND_URL}${image.url}`} alt={`news-pic-${index}`} />}
+                                {image && <img src={image.url} alt={`news-pic-${index}`} />}
                                 {lien_youtube && <p>{lien_youtube}</p>}
                                 <div className="news-text">
                                     <h2>{titre}</h2>
