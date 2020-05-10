a91f197c884a3e78ab3fa213cf3548fbdfa50124
baptiste <baptiste.bordet@gmail.com>
2020-05-10 20:35:35 +0200

    update Bio

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -6,7 +6,8 @@ import "./index.scss";
 const Bio = ({ bio }) => {
     return (
         <div className="bio-wrapper">
-            <div>{bio.description}</div>
+            <div className="description" dangerouslySetInnerHTML={{__html: bio.description}}/>
+
             {/*<img src={`${process.env.REACT_APP_BACKEND_URL}${bio.image.url}`} alt="bio-pic" />*/}
             <div className="bio-members">
                 <div>Chant – Aurélien DEJEAN</div>
@@ -15,6 +16,7 @@ const Bio = ({ bio }) => {
                 <div>Basse – Joffrey DESCHODT</div>
                 <div>Clavier – Christophe MAILLARD</div>
                 <div>Trompette – Charly JACQUET</div>
+                <div>Trompette – Baptiste BORDET</div>
             </div>
         </div>
     );
